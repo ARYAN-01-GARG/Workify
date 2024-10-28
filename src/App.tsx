d630b14d8ce58ccabda746ca0172bfeb370991d9
ARYAN-01-GARG <aryangarg30842@gmail.com>
2024-10-28 19:36:11 +0530

    Added Auth Structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router-dom"
+import LandingPage from "./pages/LandingPage"
+import AuthPage from "./pages/auth/AuthPage"
+import RegisterPage from "./pages/auth/RegisterPage"
 
 const App = () => {
   return (
-    <div className="text-2xl text-green-600">Workify</div>
+    <Routes>
+      {/* Landing page/ not protected  */}
+      <Route index element={<LandingPage />} />
+
+      {/* Auth routes  */}
+      <Route path="/auth" element={<AuthPage/>}>
+        <Route path="register" element={<RegisterPage/>} />
+        <Route path="login" element={<div>Login</div>} />
+      </Route>
+
+      {/* Protected routes */}
+      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+    </Routes>
   )
 }
 

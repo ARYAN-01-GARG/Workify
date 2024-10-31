8e2b448a96ee10114563d779d850905051d23158
ARYAN-01-GARG <aryangarg30842@gmail.com>
2024-10-31 10:21:31 +0530

    Register page finished.

--- src/components/auth/Input.tsx
+++ src/components/auth/Input.tsx
@@ -1,7 +1,106 @@
+import { RefObject, useId } from "react";
+import { AiOutlineExclamationCircle } from "react-icons/ai";
+import { BsEye, BsEyeSlash } from "react-icons/bs";
 
-const Input = () => {
+interface InputProps{
+    type? : string;
+    ref : RefObject<HTMLInputElement>;
+    label : string;
+    value : string;
+    onChange : (value : string) => void;
+    disabled? : boolean;
+    errors : string | undefined;
+    showPassword? : boolean;
+    setShowPassword? : (value : boolean) => void;
+}
+
+
+
+const Input:React.FC<InputProps> = ({
+    type = 'text',
+    label,
+    ref,
+    value,
+    onChange,
+    disabled=false,
+    errors,
+    showPassword,
+    setShowPassword
+}) => {
+
+    const handleTogglePassword = () => {
+        if(setShowPassword){
+            setShowPassword(!showPassword);
+        }
+    };
+
+    const id = useId();
+
   return (
-    <div>Input</div>
+    <div className="w-full relative">
+        <input
+            ref={ref}
+            id={id}
+            type={showPassword ? 'text' : type}
+            disabled={disabled}
+            value={value}
+            onChange={e => onChange(e.target.value)}
+            placeholder=""
+            className={`
+                peer
+                w-full
+                p-3
+                pt-4
+                font-light
+                bg-white
+                border-2
+                rounded-lg
+                outline-none
+                transition
+                diasbled:opacity-70
+                disabled:cursor-not-allowed
+                disabled:bg-gray-200
+                ${type === 'password' && !showPassword ? 'font-bold text-2xl' : ''}
+                ${errors ? 'border-red-500' : 'border-neutral-500'}
+                ${errors ? 'focus:border-red-500' : 'focus:border-[#2B5A9E]'}
+        `}
+        />
+        {label === 'Password' && (
+        <div
+          className="absolute top-4 right-4 cursor-pointer transition duration-150"
+          onClick={handleTogglePassword}
+        >
+          {!showPassword ? (
+            <BsEyeSlash size={22} />
+          ) : (
+            <BsEye size={22} />
+          )}
+        </div>
+      )}
+        <label htmlFor={id} className={`
+            absolute
+            text-sm
+            text-neutral-600
+            duration-150
+            transform
+            -translate-y-7
+            bg-white
+            px-1
+            top-4
+            left-4
+            z-10
+            orgin-[0]
+            peer-placeholder-shown:scale-110
+            peer-placeholder-shown:translate-y-0
+            peer-focus:-translate-y-7
+            ${errors ? 'text-red-500' : 'text-zinc-500'}
+            ${errors ? 'peer-focus:text-red-500' : 'peer-focus:text-[#2B5A9E]'}`}>
+            {label}
+        </label>
+        <div className={`${errors ? 'mt-2' : '' }`}>
+            {errors && <span className="text-red-400 bg-rose-100 rounded-xl p-1 text-sm px-2 mx-3 flex justify-start items-center gap-1"><AiOutlineExclamationCircle size={22}/> {errors}</span>}
+        </div>
+    </div>
   )
 }
 
--- src/pages/auth/RegisterPage.tsx
+++ src/pages/auth/RegisterPage.tsx
@@ -1,12 +1,82 @@
 import { Link } from "react-router-dom"
 import Modal from "../../components/Auth/Modal"
+import axios from "axios";
+import Input from "../../components/Auth/Input";
+import { useState, useRef, useEffect } from "react";
+
+
+interface Errors{
+  nameError: string
+  contactError: string
+  passwordError: string
+}
 
 const RegisterPage = () => {
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [errors , setErrors] = useState<Errors>({ nameError: '', contactError: '', passwordError: ''});
+
+  const [name, setName] = useState<string>('');
+  const NAME_REGEX = /^[a-zA-Z ]{3,22}$/;
+  const nameRef = useRef<HTMLInputElement>(null);
+
+
+  const [contact, setContact] = useState<string>('');
+  const contactRef = useRef<HTMLInputElement>(null);
+  const PHONE_REGEX = /^[0-9]{10}$/;
+  const EMAIL_REGEX = /^[a-zA-Z]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [password, setPassword] = useState<string>('');
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    nameRef.current?.focus();
+  },[])
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const isContactValid = EMAIL_REGEX.test(contact) || PHONE_REGEX.test(contact);
+    if(!NAME_REGEX.test(name) || !isContactValid || password.length < 6){
+      setErrors({
+        nameError: (name.length < 3) && /[a-zA-Z]/.test(name) ? 'Name is invalid must be between 2 to 22 characters' : (!NAME_REGEX.test(name)) ? 'Name is invalid' : '',
+        contactError: (!isContactValid) ?  /^[0-9]{10}$/.test(contact) ? 'Phone number is invalid' : 'Email is invalid.' : '',
+        passwordError: (password.length < 6) ? 'Password must be at least 6 characters' : ''
+      })
+    }
+    if(!name || !contact || !password){
+      setErrors({
+        nameError: !name ? 'Name is required' : '',
+        contactError: !contact ? 'Email or Phone Number is required' : '',
+        passwordError: !password ? 'Password is required' : ''
+      })
+      return;
+    }
+    else if(NAME_REGEX.test(name) || isContactValid || password.length > 6){
+      setErrors({
+        nameError: '',
+        contactError: '',
+        passwordError: ''
+      })
+    }
+    // Api logic
+    setIsLoading(true);
+    try{
+      // API call
+      const response = await axios.post('http://localhost:3000/auth/login', {contact, password});
+      console.log(response.data);
+    } catch (error){
+      console.log(error);
+    } finally{
+      setIsLoading(false);
+    }
+  }
 
   const footer = (
     <p className="text-sm -mt-7">
       Already have an account?{" "}
-      <Link to="/auth/login" className="text-[.95rem] text-[#2B5A9E] font-semibold">
+      <Link to="/auth/login" className={`text-[.95rem] text-[#2B5A9E] font-semibold ${isLoading ? 'opacity-70' : ''}`}>
         Log in
       </Link>
     </p>
@@ -14,14 +84,44 @@ const RegisterPage = () => {
   return (
     <>
       <Modal
+        disabled={isLoading}
         backURL="/"
         title="Create Your Account"
         subTitlte="Join the Workify community to find your ideal job fit."
         actionLabel="Create Account"
-        action={() => {}}
+        onSubmit={handleSubmit}
         footer={footer}
       >
-        <div className="border border-red-500 p-10">Main Body</div>
+        <form className="flex flex-col gap-5 w-full" onSubmit={handleSubmit}>
+          <Input
+            ref={nameRef}
+            label='Name'
+            value={name}
+            onChange={setName}
+            disabled={isLoading}
+            errors={errors.nameError}
+          />
+          <Input
+            ref={contactRef}
+            label={/[0-9]/.test(contact) && !/[a-zA-Z]/.test(contact) ? 'Phone' : /[a-zA-Z]/.test(contact) ? 'Email' : 'Email or Phone number'}
+            value={contact}
+            onChange={setContact}
+            disabled={isLoading}
+            type="text"
+            errors={errors.contactError}
+          />
+          <Input
+            ref={passwordRef}
+            label='Password'
+            value={password}
+            onChange={setPassword}
+            type="password"
+            disabled={isLoading}
+            errors={errors.passwordError}
+            showPassword={showPassword}
+            setShowPassword={setShowPassword}
+            />
+        </form>
       </Modal>
     </>
   )

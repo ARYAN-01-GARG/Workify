e56ed5a0725e1e852c5979c5846888a1cf039ca8
ARYAN-01-GARG <aryangarg30842@gmail.com>
2024-10-31 09:12:07 +0530

    Modal Designing Completed.

--- src/components/auth/Modal.tsx
+++ src/components/auth/Modal.tsx
@@ -19,27 +19,31 @@ const Modal:React.FC<ModalProps> = ({
   children,
   actionLabel,
   action,
-  footer
+  footer,
 }) => {
   return (
-    <div
-      className="h-full w-full ">
-        <div>
-          <Link to={'/'}>←</Link>
-        </div>
-        <div>
-          <h1>{title}</h1>
-          <p>{subTitlte}</p>
-        </div>
-        <div>
-          {children}
-        </div>
-        <div>
-          <button onClick={action}>{actionLabel}</button>
-        </div>
-        <div>
-          {footer}
-        </div>
+    <div className="relative h-full w-full flex justify-center items-center">
+      <div
+        className="flex flex-col justify-center items-center gap-12 max-w-[385px]">
+          <div className="absolute top-10 right-20 text-4xl ">
+            <Link to={backURL}>←</Link>
+          </div>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-semibold">{title}</h1>
+            <h2 className="text-[.95rem] font-medium pl-1 pr-4">{subTitlte}</h2>
+          </div>
+          <div>
+            {children}
+          </div>
+          <div className="w-full bg-[#2B5A9E] text-white text-xl font-medium text-center p-[.6rem] shadow-sm rounded-xl">
+            <button onClick={action}>
+              {actionLabel}
+            </button>
+          </div>
+          <div>
+            {footer}
+          </div>
+      </div>
     </div>
   )
 }
--- src/pages/auth/RegisterPage.tsx
+++ src/pages/auth/RegisterPage.tsx
@@ -1,17 +1,28 @@
+import { Link } from "react-router-dom"
 import Modal from "../../components/Auth/Modal"
 
 const RegisterPage = () => {
+
+  const footer = (
+    <p className="text-sm -mt-7">
+      Already have an account?{" "}
+      <Link to="/auth/login" className="text-[.95rem] text-[#2B5A9E] font-semibold">
+        Log in
+      </Link>
+    </p>
+  )
   return (
     <>
-      <Modal 
+      <Modal
         backURL="/"
         title="Create Your Account"
-        subTitlte="Join the Workify community to find your ideal job fit"
+        subTitlte="Join the Workify community to find your ideal job fit."
         actionLabel="Create Account"
         action={() => {}}
-        footer={<></>}
-      />
-      <div>Main Body</div>
+        footer={footer}
+      >
+        <div className="border border-red-500 p-10">Main Body</div>
+      </Modal>
     </>
   )
 }

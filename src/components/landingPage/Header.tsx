de2f52036f4571a758b8485437ab1558d088f12f
ARYAN-01-GARG <aryangarg30842@gmail.com>
2024-11-14 20:52:45 +0530

    APIs route change created.

--- src/components/landingPage/Header.tsx
+++ src/components/landingPage/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="sticky top-0 flex justify-between items-center px-10 py-4 bg-white md:gap-10">
       <div className="py-2 scale-105">
-        <img src="/public/images/Workify.svg" alt="logo" />
+        <img src="/images/Workify.svg" alt="logo" />
       </div>
       <nav className="flex justify-between items-center px-5 min-w-[65vw]">
         <NavBar elements={["Home", "Find Jobs", "Find Candidates", "For Recruiters" , "Career Advice"]} />

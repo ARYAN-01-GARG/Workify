084578752ec03eb7add7d67308023b8cabf31895
ARYAN-01-GARG <aryangarg30842@gmail.com>
2024-11-19 14:58:38 +0530

    Landing Page: minor bug fixed.

--- src/components/landingPage/Card.tsx
+++ src/components/landingPage/Card.tsx
@@ -11,7 +11,7 @@ const Card:React.FC<CardProps> = ({
     description,
 }) => {
   return (
-    <div className="px-5 bg-white w-[173px] h-[71px] lg:min-w-[21vw] lg:min-h-[118px] flex justify-start items-center gap-5 rounded-lg shadow-md">
+    <div className="px-5 bg-white w-[167px] h-[71px] lg:min-w-[21vw] lg:min-h-[118px] flex justify-start items-center gap-5 rounded-lg shadow-md">
         <div className="bg-[#E7F0FA] p-[3px] lg:p-5 rounded-md">
             <img src={imageURL} alt="Image" className={`w-full h-full ${imageURL === '/images/landing-page/briefcase.svg' ? '' : 'my-1 mr-2'} `}/>
         </div>

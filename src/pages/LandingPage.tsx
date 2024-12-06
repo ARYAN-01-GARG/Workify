42758598da85046012388d4349f85313ba85fcf1
ARYAN-01-GARG <aryangarg30842@gmail.com>
2024-12-06 17:16:35 +0530

    Missing Page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import { FaArrowRight, FaLocationDot, FaRegCircle } from "react-icons/fa6"
+import { FaArrowRight, FaLocationDot, FaRegCircle, FaRegCircleCheck } from "react-icons/fa6"
 import Footer from "../components/landingPage/Footer"
 import Header from "../components/landingPage/Header"
 import { FaSearch } from "react-icons/fa"
@@ -7,8 +7,8 @@ import Card2 from "../components/landingPage/Card2"
 import Card3 from "../components/landingPage/Card3"
 import { FcGoogle } from "react-icons/fc"
 import { useDispatch, useSelector } from "react-redux"
-import { Outlet, useNavigate } from "react-router-dom"
-import { useEffect } from "react"
+import { Link, Outlet, useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react"
 import { UserState } from "../store/features/auth/UserState"
 import RoleSelection from "../components/profile/RoleSelection"
 import CandidateDetails from "../components/profile/CandidateDetails"
@@ -21,6 +21,14 @@ const LandingPage = () => {
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
 
+  const [selectedType , setSelectedType] = useState<string>('Full Time');
+  const [viewAll , setViewAll] = useState<boolean>(false);
+  const [isLocked , setIsLocked] = useState<boolean>(false);
+
+  const handleSelectChange = (selected : string) => {
+    setSelectedType(selected);
+  }
+
   const isOpen = useSelector((state: { roleSelection : RoleSelectionState}) => state.roleSelection.isOpen);
   const isCandidateOpen = useSelector((state: { candidate : {isCandidateOpen : boolean}}) => state.candidate.isCandidateOpen);
   const isRecruiterOpen = useSelector((state: { recruiter : {isRecruiterOpen : boolean}}) => state.recruiter.isRecruiterOpen);
@@ -30,6 +38,7 @@ const LandingPage = () => {
 
 
   useEffect(() => {
+    setIsLocked(false);
     dispatch(getStats())
     if(IsAuthenticated && role ==='user'){
       dispatch(setIsOpen(true));
@@ -64,7 +73,7 @@ const LandingPage = () => {
             </h3>
             <div className="flex items-center py-2 border-2 border-[#3C74BB] pl-2 lg:pl-4 pr-2 gap-2 rounded-2xl">
               <div className="p-2 rounded-full bg-[#2B5A9E] text-white hover:opacity-80 cursor-pointer">
-                <FaSearch size={18}/>
+                <FaSearch size={18} onClick={() => setIsLocked(true)}/>
               </div>
               <input
                 type="text"
@@ -85,14 +94,23 @@ const LandingPage = () => {
               <span> Animation</span>.
             </div>
             <div className="flex justify-evenly lg:justify-between gap-2 lg:gap-3 items-center flex-wrap md:flex-nowrap">
-              <button className="bg-[#C8D8EF] text-[#2B5A9E] text-sm lg:text-[1rem] font-medium p-2 md:px-3 md:py-2 rounded-xl flex gap-2 items-center">
-                <FaRegCircle size={20} className="text-[#213E6B]"/>Full Time
+              <button onClick={() => handleSelectChange('Full Time')} className="bg-[#C8D8EF] text-[#2B5A9E] text-sm lg:text-[1rem] font-medium p-2 md:px-3 md:py-2 rounded-xl flex gap-2 items-center">
+                {selectedType === 'Full Time' ?
+                  <FaRegCircleCheck  size={20} className="text-[#213E6B]"/> :
+                  <FaRegCircle  size={20} className="text-[#213E6B]"/> 
+                }<span>Full Time</span>
               </button>
-              <button className="bg-[#C8D8EF] text-[#2B5A9E] text-sm lg:text-[1rem] font-medium p-2 md:px-3 md:py-2 rounded-xl flex gap-2 items-center">
-                <FaRegCircle size={20} className="text-[#213E6B]"/>Part Time
+              <button onClick={() => handleSelectChange('Part Time')} className="bg-[#C8D8EF] text-[#2B5A9E] text-sm lg:text-[1rem] font-medium p-2 md:px-3 md:py-2 rounded-xl flex gap-2 items-center">
+                {selectedType === 'Part Time' ?
+                  <FaRegCircleCheck  size={20} className="text-[#213E6B]"/> :
+                  <FaRegCircle  size={20} className="text-[#213E6B]"/>
+                }<span>Part Time</span>
               </button>
-              <button className="bg-[#C8D8EF] text-[#2B5A9E] text-sm lg:text-[1rem] font-medium p-2 md:px-3 md:py-2 rounded-xl flex gap-2 items-center">
-                <FaRegCircle size={20} className="text-[#213E6B]"/>Remote
+              <button onClick={() => handleSelectChange('Remote')} className="bg-[#C8D8EF] text-[#2B5A9E] text-sm lg:text-[1rem] font-medium p-2 md:px-3 md:py-2 rounded-xl flex gap-2 items-center">
+                {selectedType === 'Remote' ?
+                  <FaRegCircleCheck  size={20} className="text-[#213E6B]"/> :
+                  <FaRegCircle  size={20} className="text-[#213E6B]"/>
+                }<span>Remote</span>
               </button>
             </div>
           </div>
@@ -112,19 +130,21 @@ const LandingPage = () => {
               <div className="text-xl lg:text-[2.5rem] font-medium">
                 Popular category
               </div>
-              <button className="text-sm lg:text-[1rem] flex gap-2 text-[#2B5A9E] font-medium py-3 px-8 border border-[#C8D8EF] items-center hover:bg-[#fafafb]">
-                View All <FaArrowRight size={20} className="font-[100] text-[#487ac1]"/>
+              <button className="hidden lg:flex text-sm lg:text-[1rem] gap-2 text-[#2B5A9E] font-medium py-3 px-8 border border-[#C8D8EF] items-center hover:bg-[#fafafb]" onClick={() => setViewAll(prev => !prev)}>
+                {viewAll ? 'View Less': 'View All'}<FaArrowRight size={20} className="font-[100] text-[#487ac1]"/>
               </button>
             </div>
-            <div className="flex justify-evenly items-center px-3 lg:px-16 flex-wrap gap-3 lg:gap-10 pb-20 shadow-md">
+            <div className="flex justify-evenly items-center px-3 lg:px-16 flex-wrap gap-3 lg:gap-10 pb-20 shadow-md transition duration-1000">
               <Card2 imageURL={'/images/landing-page/pen.png'} title={'Graphics & Design'} description=""/>
               <Card2 imageURL={'/images/landing-page/code.png'} title={'Code & Programing'} description=""/>
               <Card2 imageURL={'/images/landing-page/megaphone.png'} title={'Digital Marketing'} description=""/>
               <Card2 imageURL={'/images/landing-page/monitor.png'} title={'Video & Animation'} description=""/>
-              <Card2 imageURL={'/images/landing-page/music.png'} title={'Music & Audio'} description="" className={'lg:flex hidden'}/>
-              <Card2 imageURL={'/images/landing-page/chart-bar.png'} title={'Account & Finance'} description="" className={'lg:flex hidden'}/>
-              <Card2 imageURL={'/images/landing-page/first-aid.png'} title={'Health & Care'} description="" className={'lg:flex hidden'}/>
-              <Card2 imageURL={'/images/landing-page/database.png'} title={'Data & Science'} description="" className={'lg:flex hidden'}/>
+              {viewAll && <>
+                <Card2 imageURL={'/images/landing-page/music.png'} title={'Music & Audio'} description="" className={'lg:flex hidden'}/>
+                <Card2 imageURL={'/images/landing-page/chart-bar.png'} title={'Account & Finance'} description="" className={'lg:flex hidden'}/>
+                <Card2 imageURL={'/images/landing-page/first-aid.png'} title={'Health & Care'} description="" className={'lg:flex hidden'}/>
+                <Card2 imageURL={'/images/landing-page/database.png'} title={'Data & Science'} description="" className={'lg:flex hidden'}/>
+              </>}
             </div>
           </div>
         </section>
@@ -153,14 +173,25 @@ const LandingPage = () => {
         </section>
       </main>
       <Footer/>
-      {(isOpen || isCandidateOpen || isRecruiterOpen) &&
+      {(isOpen || isCandidateOpen || isRecruiterOpen || isLocked) &&
         <div className="absolute min-h-screen w-full flex justify-center items-center z-50 bg-neutral-800/80 ">
           {isOpen && <RoleSelection/>}
           {isCandidateOpen && <CandidateDetails/>}
           {isRecruiterOpen && <RecruiterDetails/>}
+          {isLocked && <LockedPage/>}
         </div>
       }
       <Outlet/>
+    </div>
+  )
+}
+
+const LockedPage = () => {
+  return (
+    <div className="bg-white p-10 rounded-xl flex flex-col items-center gap-2">
+      <h1 className="text-2xl font-medium">Finally Last step to have a job.</h1>
+      <h1 className="text-xl text-black/80">Please Register to continue.</h1>
+      <Link to={'/auth/register'} className="mt-4 text-xl hover:opacity-80 text-white font-semibold bg-[#2B5A9E] px-4 py-2 rounded-xl">Sign up</Link>
     </div>
   )
 }
---
+++ src/pages/MissingPage.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+
+const MissingPage = () => {
+  return (
+    <div className="font-semibold h-screen flex flex-col gap-4 justify-center items-center text-4xl flex-wrap bg-[#E7EDF8]">
+       <p className="font-semibold">This Page is Secured in <span className="text-[#2B5A9E]">Vault</span> in void.</p>
+       <p>Get Access to Visit</p>
+       <div className="gap-4 flex text-2xl font-bold text-white mt-5">
+        <Link to={'/auth/login'} className="hover:opacity-80 bg-[#2B5A9E] px-4 py-2 rounded-xl">Login</Link>
+        <Link to={'/auth/register'} className="hover:opacity-80 bg-[#2B5A9E] px-4 py-2 rounded-xl">Sign up</Link>
+       </div>
+    </div>
+  )
+}
+
+export default MissingPage
